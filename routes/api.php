a98a22f4a3622ef030bf3eb6d0175e3299839b2a
mert <mert.mustehlil@asiste.com.tr>
2025-10-31 20:43:51 +0300

    refactor: remove v1 prefix from API endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\AuthController;
 
-Route::group(['prefix' => 'v1'], function () {
-    Route::group(['prefix' => 'auth'], function () {
-        Route::post('register', [AuthController::class, 'register']);
-        Route::post('login', [AuthController::class, 'login']);
-    });
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
 });
