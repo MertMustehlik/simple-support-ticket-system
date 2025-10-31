b2b30ce1e439d297c1b33857b3c35ee68c4f6117
mert <mert.mustehlil@asiste.com.tr>
2025-11-01 02:05:01 +0300

    refactor: edit global exception handling for AuthenticationException

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\EnsureTicketOwner;
 use Illuminate\Http\Exceptions\ThrottleRequestsException;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -20,6 +21,10 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'ticket.owner' => EnsureTicketOwner::class
         ]);
+
+        $middleware->redirectGuestsTo(function (Request $request) {
+            throw new AuthenticationException();
+        });
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (ValidationException $e): JsonResponse {
@@ -29,7 +34,7 @@ return Application::configure(basePath: dirname(__DIR__))
             ], 422);
         });
 
-        $exceptions->render(function (AuthenticationException $e): JsonResponse {
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
             return response()->json([
                 'message' => "Giriş yapılmamış!"
             ], 401);
